3076286b8449f07b80cb457627f38fbdb53ff587
kiv1n <aloha@kiv1n.ru>
2023-02-19 13:15:04 +0300

    Add functions to get account data and goals from subgraph

--- src/__tests__/e2e/integration.spec.ts
+++ src/__tests__/e2e/integration.spec.ts
@@ -1,31 +1,59 @@
 import { PolywrapClient } from "@polywrap/client-js";
-import * as App from "../types/wrap";
 import path from "path";
+import * as App from "../types/wrap";
 
 jest.setTimeout(60000);
 
-describe("Template Wrapper End to End Tests", () => {
-
-  const client: PolywrapClient = new PolywrapClient();
+describe("Wrapper End to End Tests", () => {
   let wrapperUri: string;
+  let client: PolywrapClient = new PolywrapClient();
 
   beforeAll(() => {
     const dirname: string = path.resolve(__dirname);
     const wrapperPath: string = path.join(dirname, "..", "..", "..");
     wrapperUri = `fs/${wrapperPath}/build`;
-  })
+  });
+
+  beforeEach(() => {
+    client = new PolywrapClient();
+  });
 
   it("calls sampleMethod", async () => {
     const expected: string = "polywrap";
-
     const result = await client.invoke<App.Template_SampleResult>({
       uri: wrapperUri,
       method: "sampleMethod",
-      args: { arg: expected }
+      args: { arg: expected },
     });
-
     expect(result.ok).toBeTruthy();
     if (!result.ok) return;
     expect(result.value.result).toEqual(expected);
+  });
+
+  it("calls getAccountData", async () => {
+    const result = await client.invoke({
+      uri: wrapperUri,
+      method: "getAccountData",
+      args: {
+        subgraphAuthor: "kiv1n",
+        subgraphName: "web3-goals",
+        account: "0x4306D7a79265D2cb85Db0c5a55ea5F4f6F73C4B1",
+      },
+    });
+    expect(result.ok).toBeTruthy();
+  });
+
+  it("calls getAccountGoals", async () => {
+    const result = await client.invoke({
+      uri: wrapperUri,
+      method: "getAccountGoals",
+      args: {
+        subgraphAuthor: "kiv1n",
+        subgraphName: "web3-goals",
+        account: "0x4306D7a79265D2cb85Db0c5a55ea5F4f6F73C4B1",
+      },
+    });
+    console.log(result);
+    expect(result.ok).toBeTruthy();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,62 @@
-import { Args_sampleMethod, SampleResult } from "./wrap";
+import {
+  Args_getAccountData,
+  Args_getAccountGoals,
+  Args_sampleMethod,
+  SampleResult,
+  Subgraph_Module,
+} from "./wrap";
+import { JSON } from "@polywrap/wasm-as";
 
 export function sampleMethod(args: Args_sampleMethod): SampleResult {
   return {
     result: args.arg,
   };
+}
+
+export function getAccountData(args: Args_getAccountData): JSON.Value {
+  // Query subgraph
+  const query = `{\naccount(id: "${args.account.toLowerCase()}") {\nid\nachievedGoals\nfailedGoals\nmotivatedGoals\n}\n}`;
+  const response = Subgraph_Module.querySubgraph({
+    subgraphAuthor: args.subgraphAuthor,
+    subgraphName: args.subgraphName,
+    query: query,
+  }).unwrap();
+  const json = JSON.parse(response);
+  // Check errors
+  if (!json.isObj) {
+    throw new Error(
+      "Subgraph response is not an object.\n" +
+        `Author: ${args.subgraphAuthor}\n` +
+        `Subgraph: ${args.subgraphName}\n` +
+        `Account: ${args.account}\n` +
+        `Response: ${response}`
+    );
+  }
+  // Return result
+  const obj = json as JSON.Obj;
+  return obj.valueOf().get("data") as JSON.Value;
+}
+
+export function getAccountGoals(args: Args_getAccountGoals): JSON.Value {
+  // Query subgraph
+  const query = `{\ngoals(where: {authorAddress: "${args.account.toLowerCase()}"}) {\nid\nuri\nisClosed\nisAchieved\n}\n}`;
+  const response = Subgraph_Module.querySubgraph({
+    subgraphAuthor: args.subgraphAuthor,
+    subgraphName: args.subgraphName,
+    query: query,
+  }).unwrap();
+  const json = JSON.parse(response);
+  // Check errors
+  if (!json.isObj) {
+    throw new Error(
+      "Subgraph response is not an object.\n" +
+        `Author: ${args.subgraphAuthor}\n` +
+        `Subgraph: ${args.subgraphName}\n` +
+        `Account: ${args.account}\n` +
+        `Response: ${response}`
+    );
+  }
+  // Return result
+  const obj = json as JSON.Obj;
+  return obj.valueOf().get("data") as JSON.Value;
 }
